1eb984bc0d02f7b7ea1199246a011df7bdabb245
Abhaysoft-inc <av385735@gmail.com>
2023-11-02 09:54:33 +0530

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { NewPostComponent } from './new-post/new-post.component';
+
+
+
+const routes: Routes = [
+    { path: '', redirectTo: 'blog-list', pathMatch: 'full' },
+  { path: 'blog-list', component: BlogListComponent },
+  // { path: 'blog-detail', component: BlogDetailComponent },
+  { path: 'blog-detail/:id', component: BlogDetailComponent },
+  { path: 'edit-post/:id', component: EditPostComponent },
+  { path: 'new-post', component: NewPostComponent},
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy,RouterModule } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { TrustedHtmlPipe } from './trusted-html.pipe';
+import { FormsModule } from '@angular/forms';
+import { NewPostComponent } from './new-post/new-post.component';
+
+
+@NgModule({
+  declarations: [NewPostComponent,AppComponent,BlogListComponent,BlogDetailComponent,TrustedHtmlPipe,EditPostComponent],
+  imports: [FormsModule,RouterModule,AppRoutingModule,BrowserModule, IonicModule.forRoot(), HttpClientModule,],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
